dd87b917f5886dfbc69ef4c215775ba6dbd10f39
ganning127 <ganningxu@gmail.com>
2026-01-24 15:39:58 -0500

    adding

--- routes/airports.js
+++ routes/airports.js
@@ -48,13 +48,59 @@ router.post("/", authenticateToken, requireAdmin, async (req, res) => {
 
 router.get("/", authenticateToken, async (req, res) => {
   try {
-    const { data, error } = await supabase
-      .from("Airport")
-      .select("icao_code, name")
-      .order("name", { ascending: true }); // Alphabetical order is usually best for dropdowns
+    const query = req.query.q;
 
-    if (error) throw error;
-    res.json(data);
+    if (!query) {
+      // no query, so return the last 5 airports used by the user
+      const { data, error } = await supabase.rpc(
+        "get_recent_photo_airports_by_user",
+        {
+          p_user_id: req.user.id,
+        },
+      );
+
+      console.log("Recent airports data:", data, "error:", error);
+
+      if (error) {
+        throw error;
+      } else if (data.length == 0) {
+        // user has no recent airports, return 5 airports
+        const { data: airports, error: airportsError } = await supabase
+          .from("Airport")
+          .select("icao_code, name")
+          .order("name", { ascending: true })
+          .limit(5);
+
+        if (airportsError) throw airportsError;
+        return res.json(airports);
+      } else {
+        return res.json(data);
+      }
+    }
+
+    // user passed in a query for airport search
+    if (query.startsWith("K") && query.length >= 4) {
+      // likely a US airport, search by ICAO code
+      console.log("Searching by ICAO code", query);
+      const { data, error } = await supabase
+        .from("Airport")
+        .select("icao_code, name")
+        .ilike("icao_code", `${query}%`)
+        .order("name", { ascending: true })
+        .limit(5);
+      if (error) throw error;
+      return res.json(data);
+    } else {
+      // search by name
+      const { data, error } = await supabase
+        .from("Airport")
+        .select("icao_code, name")
+        .ilike("name", `%${query}%`)
+        .order("name", { ascending: true })
+        .limit(5);
+      if (error) throw error;
+      return res.json(data);
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
