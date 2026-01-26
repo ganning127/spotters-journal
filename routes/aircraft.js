831b529cb89e1cee564295c3e513e6e1c806aaf7
ganning127 <ganningxu@gmail.com>
2026-01-25 21:25:29 -0500

    added pre population

--- routes/aircraft.js
+++ routes/aircraft.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const supabase = require("../db");
 const authenticateToken = require("../middleware/authMiddleware");
 
+const AIRLABS_API_KEY = process.env.AIRLABS_API_KEY;
+const AIRLABS_BASE_URL = process.env.AIRLABS_BASE_URL;
+
 router.get("/search", authenticateToken, async (req, res) => {
   const query = req.query.q;
 
@@ -16,7 +19,6 @@ router.get("/search", authenticateToken, async (req, res) => {
       .from("SpecificAircraft")
       .select(
         `
-        registration, 
         type_id,
         Photo!left (         
           image_url, 
@@ -25,19 +27,65 @@ router.get("/search", authenticateToken, async (req, res) => {
         )
       `,
       )
-      .ilike("registration", `${query}%`)
+      .eq("registration", query.toUpperCase())
       .eq("Photo.user_id", req.user.id)
-      .limit(10);
-
-    // restrict data.Photo to only the first photo (if any)
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].Photo && data[i].Photo.length > 0) {
-        data[i].Photo = [data[i].Photo[0]];
-      }
-    }
+      .limit(1);
 
     if (error) throw error;
-    res.json(data);
+
+    if (data.length === 0) {
+      res.json({
+        is_new_aircraft: true,
+      });
+      return;
+    } else {
+      res.json({
+        is_new_aircraft: false,
+        aircraft: data[0],
+      });
+    }
+
+    res.json({
+      data: forUser,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/new-registration", authenticateToken, async (req, res) => {
+  const query = req.query.q;
+
+  /*
+  returns:
+  {
+    aircraft_type_id: string (icao code),
+    airline_code: string (icao code),
+  }
+    */
+
+  if (!query || query.length === 0) {
+    return res.json([]);
+  }
+
+  try {
+    const result = await fetch(
+      `${AIRLABS_BASE_URL}/fleets?reg_number=${query.toUpperCase()}&api_key=${AIRLABS_API_KEY}`,
+    );
+    const data = await result.json();
+    const response = data.response;
+
+    if (!response || response.length === 0) {
+      return res.json({
+        found: false,
+      });
+    } else {
+      return res.json({
+        found: true,
+        aircraft_type_id: response[0].icao,
+        airline_code: response[0].airline_icao,
+      });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
